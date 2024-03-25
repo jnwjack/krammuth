aa747bed9b707ff254879c89bca401925f53bb1d
jnwjack <jnw900@gmail.com>
2024-03-24 21:38:22 -0400

    dependencies

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const stringify = require('csv-stringify');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  console.log('hit');
+  res.send('Hello World!');
+});
 
 app.get('/thingy', (req, res) => {
-  res.send('oh no')
-})
+  res.send('oh no');
+});
+
+app.get('/download_map/:param', (req, res) => {
+  console.log(req.params);
+
+  res.json({ val: 1234 });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
